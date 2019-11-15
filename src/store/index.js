fe7b918d14ed9ea137bafa7c2e59390079891f47
hacktivate <gunawannucky@gmail.com>
2019-11-15 17:10:32 +0700

    add maps store and fetch

--- src/store/index.js
+++ src/store/index.js
@@ -1,41 +1,54 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import axios from 'axios'
-import router from '../router'
-import {verifyToken} from '../helpers/jwt'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+import router from '../router';
+import { verifyToken } from '../helpers/jwt';
 
 Vue.use(Vuex)
 
-const baseUrl = "http://34.87.107.88"
+const baseUrl = 'http://34.87.107.88';
 
 export default new Vuex.Store({
- state: {
-   isLogin: (localStorage.getItem('token') ? true : false)
- },
- mutations: {
-   SET_LOGIN(state, data) {
-     state.isLogin = data
-   }
- },
- actions: {
-   login(context, payload) {
+  state: {
+    isLogin: !!localStorage.getItem('token'),
+    tukangs: []
+  },
+  mutations: {
+    SET_LOGIN (state, data) {
+      state.isLogin = data
+    },
+    SET_TUKANGS (state, data) {
+      state.tukangs = data
+    }
+  },
+  actions: {
+    login (context, payload) {
       axios({
         method: 'POST',
         url: `${baseUrl}/user/login`,
         data: payload
       })
-      .then(({data}) => {
-        const user = verifyToken(data.token)
-        console.log(user)
-        context.commit('SET_LOGIN', data.token)
-        localStorage.setItem('token', data.token)
-        if(data.isOwner) {
-          router.push('/dashboard')
-        }
+        .then(({ data }) => {
+          const user = verifyToken(data.token)
+          // console.log(user)
+          context.commit('SET_LOGIN', data.token)
+          localStorage.setItem('token', data.token)
+          if (data.isOwner) {
+            router.push('/dashboard')
+          }
+        })
+        .catch(console.log)
+    },
+    fetchTukangs (context, payload) {
+      axios({
+        method: 'GET',
+        url: `${baseUrl}/user/`
       })
-      .catch(console.log)
-   }
- },
- modules: {
- }
+        .then(({ data }) => {
+          context.commit('SET_TUKANGS', data)
+        })
+        .catch(console.log)
+    }
+  },
+  modules: {}
 })
